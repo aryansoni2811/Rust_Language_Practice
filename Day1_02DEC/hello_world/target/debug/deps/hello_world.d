3ff42cac27338286abe151600196939fb2a9afeb
aryansoni2811 <onlyaryan2003@gmail.com>
2025-12-02 20:54:23 +0530

    02-december

---
+++ Day1_02DEC/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\onlya\Desktop\Rust\Day1_02DEC\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\onlya\Desktop\Rust\Day1_02DEC\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ Day1_02DEC/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\onlya\Desktop\Rust\Day1_02DEC\hello_world\target\debug\hello_world.exe: C:\Users\onlya\Desktop\Rust\Day1_02DEC\hello_world\src\main.rs
